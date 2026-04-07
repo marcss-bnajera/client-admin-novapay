d34ec4a017d1a8f637c730f18f9a644ba56e287f
marcss-bnajera <mbeteta-2024227@kinal.edu.gt>
2026-04-07 11:16:13 -0600

    Configuracion principal

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { ThemeProvider } from "@material-tailwind/react"
+import { BrowserRouter } from "react-router-dom"
+import '../styles/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
